4a5371862f7e5feb2636661cc2f2ef24738418a8
lovelaker007 <lovelaker7@gmail.com>
2018-09-20 18:07:56 +0800

    两个版本

---
+++ 两个版本.py
@@ -0,0 +1,13 @@
+'''
+指定python版本运行pip
+    python2 -m pip -V
+    python3 -m pip -V
+        pip 9.0.1 from C:\Python3\lib\site-packages (python 3.6)
+
+    pip list
+    pip install
+    pip uninstall
+
+虚拟环境
+    virtualenv -p C:\Python3\python3.exe venv3
+'''
